5eabb34274a4739830142265811182e098779440
ekozf <50245213+ekozf@users.noreply.github.com>
2023-01-22 03:05:44 +0100

    Load & save pages

--- EKO.StaticPageBuilder/Program.cs
+++ EKO.StaticPageBuilder/Program.cs
@@ -1,43 +1,105 @@
 ﻿using EKO.StaticPageBuilder;
 using EKO_ContentParser;
+using System.Text;
 
 var path = "D:\\Code\\Sites\\ekozf.github.io";
 
-var config = PathChecker.CheckAndConvertArgs(new string[] { path });
+var configs = PathChecker.CheckAndConvertArgs(new string[] { path });
 
-if (config is null) return;
+if (configs is null) return;
 
-var pathsAreValid = PathChecker.IsPathValid(config.InputPath) &&
-                    PathChecker.IsPathValid(config.OutputPath) &&
-                    PathChecker.IsPathValid(config.TemplatePath) &&
-                    PathChecker.IsPathValid(config.ImagesDirectoryPath);
+var pages = new List<Page>();
 
-if (pathsAreValid)
+foreach (var config in configs.Where(c => c.HasContent))
 {
-    var generator = MarkdownParser.GetParser();
+    var pathsAreValid = PathChecker.IsPathValid(config.InputPath) &&
+                        PathChecker.IsPathValid(config.OutputPath) &&
+                        PathChecker.IsPathValid(config.TemplatePath);
 
-    var results = generator
-                    .UseFileOrDirectory(config.InputPath)
-                    .ReadContents()
-                    .ParseMarkdown()
-                    .ParseYaml()
-                    .SaveFilesTo(config.OutputPath)
-                    .GetObjects();
-
-    if (results != null)
+    if (pathsAreValid)
     {
-        Console.WriteLine("Parsed " + results.Count + " files");
+        var generator = MarkdownParser.GetParser();
 
-        for (int i = 0; i < results.Count; i++)
+        var results = generator
+                        .UseFileOrDirectory(config.InputPath)
+                        .ReadContents()
+                        .ParseMarkdown()
+                        .ParseYaml()
+                        .SaveFilesTo(config.OutputPath)
+                        .GetObjects();
+
+        if (results != null)
         {
-            var result = results[i];
+            Console.WriteLine("Parsed " + results.Count + " files");
 
-            var pairs = result.YamlConfig.ToList();
+            for (int i = 0; i < results.Count; i++)
+            {
+                var page = new Page
+                {
+                    Content = results[i].FileContent!,
+                    Template = File.ReadAllText(config.TemplatePath),
+                    MetaData = results[i].MetaData,
+                    SavePath = config.OutputPath,
+                };
 
-            config.PageVars.Add(new PageVar(pairs[i].Key, pairs[i].Value));
+                page.PageVars.AddRange(results[i].YamlConfig.Select(x => new PageVar(x.Key, x.Value)));
+
+                pages.Add(page);
+            }
+
+            Console.WriteLine("Done loading in pages & variables.");
         }
-
-        //add filename, createdate, editdate
-        // remember, input for this program should be an array of PageBuilderConfig
     }
 }
+
+foreach (var page in pages)
+{
+    var builder = new StringBuilder(page.Template);
+
+    foreach (var pageVar in page.PageVars)
+    {
+        builder.Replace($"@#{pageVar.Name.ToUpper()}#@", pageVar.Value);
+    }
+
+    builder.Replace("@#CONTENT#@", page.Content);
+
+    page.GeneratedHTML = builder.ToString();
+
+    File.WriteAllText(page.SavePath + page.MetaData.FileName + ".html", page.GeneratedHTML);
+}
+
+foreach (var config in configs.Where(c => !c.HasContent))
+{
+    var paged = Helpers.PageList(pages, 5);
+
+    for (int i = 0; i < paged.Count; i++)
+    {
+        var builder = new StringBuilder(File.ReadAllText(config.TemplatePath));
+
+        var widgetList = new StringBuilder();
+
+        foreach (var page in paged[i])
+        {
+            var widget = File.ReadAllText(config.WidgetPath!);
+
+            widget = widget
+                        .Replace("@#IMAGESDIRECTORYPATH#@", config.ImagesDirectoryPath)
+                        .Replace("@#COVER#@", page.PageVars.Find(x => x.Name == "cover")?.Value ?? "code.jpg")
+                        .Replace("@#TITLE#@", page.PageVars.Find(x => x.Name == "title")?.Value ?? "No title")
+                        .Replace("@#DESCRIPTION#@", page.PageVars.Find(x => x.Name == "description")?.Value ?? "No description")
+                        .Replace("@#FILENAME#@", page.MetaData.FileName);
+
+            widgetList.Append(widget);
+        }
+
+        builder.Replace("@#WIDGETS#@", widgetList.ToString());
+
+        var t = builder.ToString();
+        var b = widgetList.ToString();
+
+        File.WriteAllText(config.OutputPath + $"{i + 1}.html", builder.ToString());
+    }
+}
+
+Console.WriteLine("Done building pages.");
+Console.ReadLine();
